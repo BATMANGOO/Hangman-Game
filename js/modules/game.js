dd020ba99eecec3d690c20341013927337502419
Prabhjot Singh <pjsingh1359@gmail.com>
2019-09-05 09:44:41 -0700

    working on game module

--- js/modules/game.js
+++ js/modules/game.js
@@ -1,6 +1,7 @@
 'use strict';
 import Home from "./home.js";
 import { sound } from '../data/sound.js';
+
 const Game = (_ => {
   const $hangman = document.querySelector('.hangman');
   const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -26,7 +27,7 @@ const Game = (_ => {
       </p>
       <h1 class='hangman__title'>Hangman</h1>
       <canvas class='hangman__board' height='155px'></canvas>
-      <div class='hangman__word'>${guessingWord.join('_')}</div>
+      <div class='hangman__word'>${guessingWord.join('')}</div>
       <p class='hangman__instructions'> Pick a letter below to guess the whole word.</p>
       <ul class='hangman__letters'>
         ${createLetters()}
@@ -40,6 +41,7 @@ const Game = (_ => {
     $hangman.addEventListener('click', event => {
       if(event.target.matches('.hangman__letter')) {
         sound.click.play()
+        check(event.target.innerHTML.toLowerCase())
       }
       if(event.target.matches('.hangman__trigger')) {
         sound.click.play();
@@ -48,15 +50,53 @@ const Game = (_ => {
     });
   };
 
+  const isAlreadyTaken = letter => {
+    return guesses.includes(letter);
+  }
+
+  const check = guess => {
+    if (isAlreadyTaken(guess))  return;
+
+    guesses.push(guess);
+
+    //check if guess exists in given word
+    if (chosenWord.includes(guess)) {
+      //update the guessing word
+      updateGuessingWord(guess);
+    } else {
+      lives--;
+      //render board
+    }
+    render()
+    //check if game is over
+  }
+
   const createLetters = _ => {
     let markup = '';
-    letters.forEach( letter => {
+    letters.forEach(letter => {
+      const isActive = isAlreadyTaken(letter) ? 'hangman__letter--active' : '';
       markup += `
-        <li class='hangman__letter'>${letter}</li>
+        <li class='hangman__letter ${isActive}'>${letter}</li>
       `
     });
    return markup; 
   };
+
+  const render = _ => {
+    document.querySelector('.hangman__lives').innerHTML = lives;
+    document.querySelector('.hangman__word').innerHTML = guessingWord.join('');
+    document.querySelector('.hangman__letters').innerHTML = createLetters();
+  };
+
+  const updateGuessingWord = letter => {
+    chosenWord.split('').forEach((elem, index) => {
+      if (elem === letter) {
+        guessingWord[index] = elem;
+      }
+    });
+  };
+
+
 
   const chooseWord = _ => {
     let randNum = Math.floor(Math.random() * words.length);
